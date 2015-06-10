5e2063b73456ee3841a147a6acbcaa8a2f27232d
Lin <jl6613@cider02.doc.ic.ac.uk>
2015-06-10 12:46:37 +0100

    add fiels

---
+++ IOS8SwiftTakePhotoTutorial/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  IOS8SwiftTakePhotoTutorial
+//
+//  Created by Lin, Jiahao on 10/06/2015.
+//  Copyright (c) 2015 Lin, Jiahao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func takePhoto(sender: UIButton) {
+        
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+
+}
+
